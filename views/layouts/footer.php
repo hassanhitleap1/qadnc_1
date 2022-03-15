512b7ded2e42d72919acae08a4e1f5e8df4b8e69
unknown <hamzeh.alnatshehgmail.com>
2022-03-15 22:56:31 +0300

    new deign

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="d-flex">
             <div class="flex-grow-1 text-right">
-                <img src="<?= Yii::$app->request->baseUrl ?>/images/ligth-logo.png" alt="qadnc logo" width="250" />
+                <img src="<?= Yii::$app->request->baseUrl ?>/images/ligth-logo.png" alt="qadnc logo" width="220" />
             </div>
             <div></div>
             <div class="align-self-center">
